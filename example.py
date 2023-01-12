0c49d4847c47cb05010b1e39c79c0a8c0d504257
tonykgill <tony.k.gill@gmail.com>
2023-01-12 04:52:19 +0000

    Added the Cibo logo and GNU GPL.

--- example.py
+++ example.py
@@ -6,6 +6,23 @@ in conjunction with the documentation for pixdrill.drill().
 
 """
 
+# This file is part of Pixel Driller - for extracting pixels from
+# imagery that correspond to survey field sites.
+# Copyright (C) 2023 Cibolabs.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import datetime
 from osgeo import osr
 import numpy
